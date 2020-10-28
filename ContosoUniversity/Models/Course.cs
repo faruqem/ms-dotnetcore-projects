0c7b3c826ded45ddd717c108360751ba890d80c7
Mo Faruqe <faruqem@yahoo.com>
2020-10-27 22:48:48 -0400

    updated app - CRUD basics

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -7,7 +7,7 @@ namespace ContosoUniversity.Models
         [DatabaseGenerated(DatabaseGeneratedOption.None)] // The DatabaseGenerated attribute allows the app to specify the primary key rather than having the database generate it.
         public int CourseID { get; set; }
         public string Title { get; set; }
-        public string Credits { get; set; }
+        public int Credits { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; } // The Enrollments property is defined as ICollection<Enrollment> because there may be multiple related Enrollment entities. You can use other collection types, such as List<Enrollment> or HashSet<Enrollment>. When ICollection<Enrollment> is used, EF Core creates a HashSet<Enrollment> collection by default.
     }
